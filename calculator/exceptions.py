639f48ccc9a03c1a27e62b7bb93f3dbc3a550b47
ali <ali.nouri213@gmail.com>
2021-05-27 15:55:01 +0430

    add directories and exceptions

---
+++ calculator/exceptions.py
@@ -0,0 +1,16 @@
+class InvalidNumberException(Exception):
+    def __init__(self, message=None):
+        self.msg = message
+
+
+class InvalidOperatorException(Exception):
+    def __init__(self, message=None):
+        self.msg = message
+
+
+class InvalidFormatException(Exception):
+    def __init__(self, message=None):
+        self.msg = message
+
+
+
